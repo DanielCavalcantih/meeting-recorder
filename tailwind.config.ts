b66d7b61b6e17f26931a71f3517cf869efa02b08
DanielCavalcantih <danielcavalcanti8000@gmail.com>
2026-03-17 14:25:29 -0300

    feat: initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4f46e5",
+        secondary: "#06b6d4",
+      },
+    },
+  },
+};
+
+export default config;
